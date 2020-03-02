cedc9cffb96c022e8e389a181570516fa05cc49c
David Ding <ding@dingstudio.cn>
2020-03-02 15:54:03 +0800

    Initial Commit

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'ur879377-53c7088d4da1c50836ecc10c'
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
